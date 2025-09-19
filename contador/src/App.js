cd649335bbb2c9c1868d5bcd97b021d201147618
VictorLemosbar <victor.escolas9045@gmail.com>
2025-09-18 22:08:34 -0300

    aplicativo finalizado

--- contador/src/App.js
+++ contador/src/App.js
@@ -1,7 +1,32 @@
 import { useState, useEffect } from "react";
 
-function Contador() {
+function MyApp() {
+  const [mensagem, setMensagem] = useState("Boa");
+  const [contadorCurtidas, setContadorCurtidas] = useState(0);
 
-} 
+  useEffect(() => {
+    document.title = `Likes: ${contadorCurtidas}`;
+  }, [contadorCurtidas]); 
 
-export default Contador
+
+  useEffect(() => {
+    if (contadorCurtidas > 5) {
+      setMensagem("Bombando");
+    } else {
+      setMensagem("Boa");
+    }
+  }, [contadorCurtidas]); 
+
+  function handleClick() {
+    setContadorCurtidas(valorAtual => valorAtual + 1);
+  }
+
+  return (
+    <div>
+      <div>{mensagem}</div>
+      <button onClick={handleClick}>Curtir</button>
+    </div>
+  );
+}
+
+export default MyApp;
